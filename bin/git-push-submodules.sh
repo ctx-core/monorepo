53661cd9beb212311d6e274bb53ebe26a75780e8
Brian Takita <brian.takita@gmail.com>
2019-12-11 21:05:20 -0500

    feat: git-push-submodules.sh: commit root monorepo

--- bin/git-push-submodules.sh
+++ bin/git-push-submodules.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+DIR="$(pwd)"
 while read dir; do
   echo "$dir"
   cd "$dir"
   git add .
   git commit -av < /dev/tty > /dev/tty
 done <<< $(git submodule foreach pwd)
+cd $DIR
+git add .
+git commit -av < /dev/tty > /dev/tty

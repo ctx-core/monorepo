c388ca22fcaf33a68f8c121ea0f96e493662d382
Brian Takita <brian.takita@gmail.com>
2019-12-14 04:00:15 -0500

    feat: bin/ensure.sh

---
+++ bin/ensure.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+function ensure {
+	PS="$(ps aux | grep -- "$(echo $@)" | grep -v grep)"
+	if [ -z "$PS" ]; then
+		echo "$@"
+		exec "$@"
+	fi
+}
+[ "$(basename $0)" = "ensure.sh" ] && ensure $@

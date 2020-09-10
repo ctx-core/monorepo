ea024d846012de00fa1d97523ebdb91573e1fd1a
Brian Takita <brian.takita@gmail.com>
2020-09-10 00:52:48 -0700

    fix: package.json: bin: - git-push-submodules
    - run_parallel_workspaces.d.ts

--- run_parallel_workspaces.d.ts
+++
@@ -1,2 +0,0 @@
-import { monorepo_thread_opts_type } from './monorepo_thread_opts_type';
-export declare function run_parallel_workspaces(cmd_a1: any, opts?: monorepo_thread_opts_type): Promise<{}>;

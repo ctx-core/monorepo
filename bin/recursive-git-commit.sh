17f65248ca27c6a8c1d036a0943420611f9a76ad
Brian Takita <brian.takita@gmail.com>
2020-09-23 16:22:28 -0700

    update dependencies

--- bin/recursive-git-commit.sh
+++ bin/recursive-git-commit.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-TEMP=$(mktemp); cat <<'EOF' > $TEMP; vim $TEMP; pnpm recursive exec --workspace-concurrency 1 -- sh "$TEMP"
+TEMP=$(mktemp)
+cat <<'EOF' > $TEMP
 DEFAULT_MSG=''
 git add .
-if [ -f COMMIT_EDITMSG ]; then
+if [[ ( -f COMMIT_EDITMSG ) && ( -n "$(cat COMMIT_EDITMSG | surrounding-trim)" ) ]]; then
   git commit -a -F COMMIT_EDITMSG && rm COMMIT_EDITMSG
 else
   git commit -am "$DEFAULT_MSG"
 fi||echo ''
 EOF
+vim $TEMP
+pnpm recursive exec --workspace-concurrency 1 -- sh "$TEMP"
